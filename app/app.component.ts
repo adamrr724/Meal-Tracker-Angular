de301b969edc46096a08623431be48ab1b37f851
Adam Ross Russell <student@epicodus.com>
2016-03-25 08:37:12 -0700

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter } from 'angular2/core';
+import { Meal } from './meal.model';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div class="container">
+      <h1>Meal Tracker</h1>
+      <meal-list>
+      </meal-list>
+    <div>
+  `
+})
+
+export class AppComponent {
+  public meals: Meal[];
+  constructor(){
+    this.meals = [
+      new Meal("Cheeseburger", "Low Fat!", 435),
+      new Meal("Pizza", "Healthiest Pizza Ever", 543),
+      new Meal("Ice Cream", "This diet's going great!", 345),
+      new Meal("Lard", "I have digressed.", 856)
+    ];
+  }
+}
